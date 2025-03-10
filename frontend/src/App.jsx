9fb0a51a048aa1985f7a1d27fee75c9d55b2dd18
Stanley Jian <jianstanley@outlook.com>
2025-03-09 20:00:03 -0700

    React components finished

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,14 +8,6 @@ function App() {
 			<h1>Hack at UCI Tech Deliverable</h1>
 
 			<h2>Submit a quote</h2>
-			{/* TODO: implement custom form submission logic to not refresh the page */}
-			<form action="/api/quote" method="post">
-				<label htmlFor="input-name">Name</label>
-				<input type="text" name="name" id="input-name" required />
-				<label htmlFor="input-message">Quote</label>
-				<input type="text" name="message" id="input-message" required />
-				<button type="submit">Submit</button>
-			</form>
 
 			<h2>Previous Quotes</h2>
 			<QuoteManager/>
--- frontend/src/components.jsx
+++ frontend/src/components.jsx
@@ -1,4 +1,35 @@
-import { Component } from "react";
+import { Component, createRef } from "react";
+
+class QuoteForm extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	submit = async (e) => {
+		e.preventDefault();
+		e.stopPropagation();
+		
+		let data = new FormData(e.target);
+		await fetch("/api/quote", {"method": "POST", body: data});
+		this.props.update();
+	}
+
+	render() {
+		return (
+<form action="/quote" method="POST" onSubmit={this.submit}>
+	<div>
+		<label htmlFor="name">Name:</label>
+		<input type="text" name="name"/>
+	</div>
+	<div>
+		<label htmlFor="message">Quote:</label>
+		<input type="text" name="message"/>
+	</div>
+	<input type="submit" value="Submit"/>
+</form>
+		);
+	}
+}
 
 class QuoteDisplay extends Component {
 	constructor(props) {
@@ -22,35 +53,51 @@ class QuoteDisplay extends Component {
 
 export class QuoteManager extends Component {
 	state = {
-		history: 1,
 		quotes: []	
 	};
 	
 	constructor(props) {
 		super(props);
+		this.historyFormRef = createRef();
+	}
+
+	loadQuotesAsync = async () => {
+		let response = await (await fetch("api/history", {method: "POST", body: new FormData(this.historyFormRef.current)})).json();
+		this.setState({quotes: response["quotes"]});
+	}
+
+	loadQuotes = (e) => {
+		e.preventDefault();
+		e.stopPropagation();
+		this.loadQuotesAsync();
 	}
 
 	render() {
 		return (
 <div>
-	<div>
-		<select>
-			<option>last day</option>
-			<option>last week</option>
-			<option>last month</option>
-			<option>last year</option>
-			<option>all</option>
+	<QuoteForm update={this.loadQuotesAsync}/>
+	<form ref={this.historyFormRef} action="/api/history" method="POST" onSubmit={this.loadQuotes}>
+		<select name="span">
+			<option value={1}>last day</option>
+			<option value={7}>last week</option>
+			<option value={30}>last month</option>
+			<option value={365}>last year</option>
+			<option value={-1}>all</option>
 		</select>
-		<button>Find</button>
-	</div>
+		<input value="Find" type="submit"/> 
+	</form>
 	<div>
 		<table>
-			<tr>
-				<th>Name</th>
-				<th>Quote</th>
-				<th>Time</th>
-			</tr>
-			{this.state.quotes.map(i => <QuoteDisplay name={i.name} quote={i.quote} time={i.time}/>)}
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th>Quote</th>
+					<th>Time</th>
+				</tr>
+			</thead>
+			<tbody>
+				{this.state.quotes.map((v, i) => <QuoteDisplay key={i} name={v.name} quote={v.message} time={new Date(Date.parse(v.time)).toLocaleString()}/>)}
+			</tbody>
 		</table>
 	</div>
 </div>
