a71b6eabfac25beaf617d4caa7340a446d0a788a
Stanley Jian <jianstanley@outlook.com>
2025-03-09 19:59:38 -0700

    Backend business logic

--- api/src/app.py
+++ api/src/app.py
@@ -1,9 +1,10 @@
 from contextlib import asynccontextmanager
-from datetime import datetime
+from datetime import datetime, timezone
 from typing import AsyncIterator
+import time
 
 from fastapi import FastAPI, Form, status
-from fastapi.responses import RedirectResponse
+from fastapi.responses import JSONResponse
 from typing_extensions import TypedDict
 
 from services.database import JSONDatabase
@@ -34,17 +35,27 @@ app = FastAPI(lifespan=lifespan)
 
 
 @app.post("/quote")
-def post_message(name: str = Form(), message: str = Form()) -> RedirectResponse:
+def post_message(name: str = Form(), message: str = Form()) -> JSONResponse:
     """
     Process a user submitting a new quote.
     You should not modify this function except for the return value.
     """
-    now = datetime.now()
+    now = datetime.now(timezone.utc) # record UTC time
     quote = Quote(name=name, message=message, time=now.isoformat(timespec="seconds"))
     database["quotes"].append(quote)
 
-    # You may modify the return value as needed to support other functionality
-    return RedirectResponse("/", status.HTTP_303_SEE_OTHER)
+    return JSONResponse({"status": "success"})
 
-
-# TODO: add another API route with a query parameter to retrieve quotes based on max age
+@app.post("/history")
+def quote_history(span: int = Form(ge=-1, le=365)) -> JSONResponse:
+	resp = []
+	ct = time.time()
+	diff = span * 86400
+	if diff >= 0:
+		for q in database["quotes"]:
+			qt = time.mktime(datetime.fromisoformat(q["time"]).timetuple())
+			if ct - qt < diff:
+				resp.append(q)
+	else:
+		resp = database["quotes"]
+	return JSONResponse({"quotes": resp})
